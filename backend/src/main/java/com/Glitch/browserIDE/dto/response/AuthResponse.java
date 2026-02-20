112b7301a325ddd4319ab04af2f28f60a337fd4b
Carbon14-48 <Carbon14-48@users.noreply.github.com>
2026-02-20 03:43:53 +0000

    DTOs Done

--- backend/src/main/java/com/Glitch/browserIDE/dto/response/AuthResponse.java
+++ backend/src/main/java/com/Glitch/browserIDE/dto/response/AuthResponse.java
@@ -1,5 +1,26 @@
 package com.Glitch.browserIDE.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
 public class AuthResponse {
 
+    private String token;
+
+    @Builder.Default
+    private String tokenType = "Bearer";
+
+    private UserDTO user;
+
+    private Long expiresIn;
+
+    public AuthResponse(String token, UserDTO user) {
+        this.token = token;
+        this.user = user;
+        this.tokenType = "Bearer";
+    }
 }
--- backend/src/main/java/com/Glitch/browserIDE/dto/response/ErrorResponse.java
+++ backend/src/main/java/com/Glitch/browserIDE/dto/response/ErrorResponse.java
@@ -1,5 +1,35 @@
 package com.Glitch.browserIDE.dto.response;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class ErrorResponse {
 
+    private Integer status;
+
+    private String message;
+
+    private String details;
+
+    private List<String> errors;
+
+    @Builder.Default
+    private LocalDateTime timestamp = LocalDateTime.now();
+
+    private String path;
+
+    public ErrorResponse(Integer status, String message, String path) {
+
+        this.status = status;
+        this.message = message;
+        this.path = path;
+        this.timestamp = LocalDateTime.now();
+    }
 }
